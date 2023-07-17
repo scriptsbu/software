784c6c3c12b3856946f52440d2894d8d4677646c
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 15:41:01 -0500

    Update paperdown.sh

--- paperdown.sh
+++ paperdown.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
-gnome-terminal --  bash <(curl -Ls https://github.com/scriptsbu/software/raw/main/papercut.sh)
+gnome-terminal -- sudo bash <(curl -Ls https://github.com/scriptsbu/software/raw/main/papercut.sh)
 google-chrome https://torc.atlassian.net/wiki/download/attachments/230037450/pc-print-deploy-client%5Bpapercut.torc.tech%5D.deb?version=2&modificationDate=1676320448853&cacheVersion=1&api=v2
