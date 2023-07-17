96300bae4739cb42689fce13ac17f09621fe10a8
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 10:39:08 -0500

    Delete office.sh

--- office.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update && upgrade -y
-sudo apt install winbind -y
-sudo apt install playonlinux -y
-sudo apt install winetricks -y
-sudo wget https://github.com/scriptsbu/software/raw/main/exe/OfficeSetup.exe -P /usr/local
-playonlinux /usr/local/OfficeSetup.exe
-sudo rm /usr/local/OfficeSetup.exe
