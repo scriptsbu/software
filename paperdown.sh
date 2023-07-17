cbdb03731bb76086cc0d28c19e33b342e5a3aab5
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 15:35:56 -0500

    Update paperdown.sh

--- paperdown.sh
+++ paperdown.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
-gnome-terminal -- read -p "Press [Enter] key to after download" && sudo apt-get install ~/Downloads/pc-print-deploy-client[papercut.torc.tech].deb -y -f && sleep 5 && rm ~/Downloads/pc-print-deploy-client[papercut.torc.tech].deb
+gnome-terminal -- sleep 3 && read -p "Press [Enter] key to after download" && sudo apt-get install ~/Downloads/pc-print-deploy-client[papercut.torc.tech].deb -y -f && sleep 5 && rm ~/Downloads/pc-print-deploy-client[papercut.torc.tech].deb
 google-chrome https://torc.atlassian.net/wiki/download/attachments/230037450/pc-print-deploy-client%5Bpapercut.torc.tech%5D.deb?version=2&modificationDate=1676320448853&cacheVersion=1&api=v2
