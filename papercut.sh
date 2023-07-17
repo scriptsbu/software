237d10d75b07cd5ea3cdfb9967da7c7328fa88fb
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 15:42:36 -0500

    Update and rename paperdown.sh to papercut.sh

--- papercut.sh
+++ papercut.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-gnome-terminal -- sudo bash <(curl -Ls https://github.com/scriptsbu/software/raw/main/papercut.sh)
+wget https://github.com/scriptsbu/software/raw/main/papercut.sh
+
 google-chrome https://torc.atlassian.net/wiki/download/attachments/230037450/pc-print-deploy-client%5Bpapercut.torc.tech%5D.deb?version=2&modificationDate=1676320448853&cacheVersion=1&api=v2
