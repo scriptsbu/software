3dcc8474c7acd30ec89901e7f903b65c3d22c355
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 15:04:46 -0500

    Update papercut.sh

--- papercut.sh
+++ papercut.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
-wget https://torc.atlassian.net/wiki/download/attachments/230037450/pc-print-deploy-client%5Bpapercut.torc.tech%5D.deb?version=2&modificationDate=1676320448853&cacheVersion=1&api=v2
+/usr/bin/open -a "/Applications/Google Chrome.app" --args 'https://torc.atlassian.net/wiki/download/attachments/230037450/pc-print-deploy-client%5Bpapercut.torc.tech%5D.deb?version=2&modificationDate=1676320448853&cacheVersion=1&api=v2'
 sudo apt-get install pc-print-deploy-client[papercut.torc.tech].deb -f -y
