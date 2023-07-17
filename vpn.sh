c901ccebbe78e0b11f9f31207680e5b6bae88fcb
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 12:07:20 -0500

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update && apt-get full-upgrade -y
-wget https://github.com/scriptsbu/software/raw/main/exe/anyconnect4.10.sh /share
-bash /share/cisco/anyconnect4.10.sh
-rm /share/cisco
+wget https://github.com/scriptsbu/software/raw/main/exe/anyconnect4.10.sh
+bash anyconnect4.10.sh
+rm anyconnect4.10.sh
