6d9618af64dccab0573a7414d06f6af56f4ae633
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 11:59:13 -0500

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get update && apt-get full-upgrade -y
-bash <(curl -Ls https://github.com/scriptsbu/software/raw/main/exe/anyconnect4.10.sh)
+bash <(curl -Ls https://github.com/scriptsbu/software/raw/main/anyconnect4.10.sh)
