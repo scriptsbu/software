482c1c7d377b35e52c295aa326c53dcd1ebf5f00
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 16:01:03 -0500

    Delete paperdown.sh

--- paperdown.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-google-chrome https://torc.atlassian.net/wiki/download/attachments/230037450/pc-print-deploy-client%5Bpapercut.torc.tech%5D.deb?version=2&modificationDate=1676320448853&cacheVersion=1&api=v2
