1f7076c16ad818316a2990ff411d433c16d39a14
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 14:56:45 -0500

    Update papercut.sh

--- papercut.sh
+++ papercut.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
-wget https://github.com/scriptsbu/software/raw/main/storage/pc-mobility-print-1.0.3541.sh
-sudo bash pc-mobility-print-1.0.3541.sh
+wget https://torc.atlassian.net/wiki/download/attachments/230037450/pc-print-deploy-client%5Bpapercut.torc.tech%5D.deb?version=2&modificationDate=1676320448853&cacheVersion=1&api=v2
+sudo apt-get install pc-print-deploy-client[papercut.torc.tech].deb -f -y
