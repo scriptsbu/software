c1216f7f5582e23d23597a07c10c4460044979a7
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-17 12:50:17 -0500

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update && apt-get full-upgrade -y
-wget https://github.com/scriptsbu/software/blob/main/storage/anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh
+wget https://github.com/scriptsbu/software/raw/main/storage/anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh
 sudo bash anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh
