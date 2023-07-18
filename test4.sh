93db3759d7ad84b49a65195ce842347c2aa38d3e
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-18 16:16:22 -0500

    Update and rename test3.sh to test4.sh

--- test4.sh
+++ test4.sh
@@ -1,3 +1,3 @@
 echo -e '\x1b[41;37mWarning text\x1b[K\x1b[0m';echo Normal text
-echo -e '\033[95mmWarning text\x1b[K\x1b[0m';echo Normal text
+echo -e '\033[35mWarning text\x1b[K\x1b[0m';echo Normal text
 
