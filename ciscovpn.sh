301a21e87ed7506f65acbfcf77cc1b5584ea0b5a
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-18 16:41:45 -0500

    Update ciscovpn.sh

--- ciscovpn.sh
+++ ciscovpn.sh
@@ -2,3 +2,4 @@
 sudo apt-get update && apt-get upgrade -f -y
 wget https://github.com/scriptsbu/software/raw/main/storage/anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh
 sudo bash anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh
+sudo rm -r anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh -f
