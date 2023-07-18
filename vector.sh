e1be7149c69519a719f87449316bfaccefacd101
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-18 16:56:52 -0500

    Delete vector.sh

--- vector.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -1sLf \
-  'https://repositories.timber.io/public/vector/cfg/setup/bash.deb.sh' \
-| sudo -E bash
-sudo apt-get install vector -y
-sudo apt-get upgrade vector -y
-vector --version
