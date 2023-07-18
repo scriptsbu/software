9f3294a37f4ce9054801205a56dd474c5aee53d6
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-18 13:14:00 -0500

    Update papercut.sh

--- papercut.sh
+++ papercut.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 read -p "A browser will launch Confluence to access a file, login, and return to this screen. Please press [ENTER] to proceed."
+google-chrome https://torc.atlassian.net/ -f
+read -p "A browser will launch Confluence to download a file; Please press [ENTER] to proceed and return to this screen."
 google-chrome https://torc.atlassian.net/wiki/download/attachments/230037450/pc-print-deploy-client%5Bpapercut.torc.tech%5D.deb?version=2&modificationDate=1676320448853&cacheVersion=1&api=v2 -f
 sleep 3
 read -p "Press [Enter] key after the download is completed."
