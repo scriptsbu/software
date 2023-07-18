21efdf4ac5d810e9b574f51bf3ac3fd003c62f40
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-18 16:18:15 -0500

    Update test5.sh

--- test4.sh
+++ test4.sh
@@ -1,3 +1,4 @@
 echo -e '\x1b[41;37mWarning text\x1b[K\x1b[0m';echo Normal text
 echo -e '\033[35mWarning text\x1b[K\x1b[0m';echo Normal text
 
+read -p '\x1b[41;37mA browser will launch Confluence to access a file, login, close the browser window, and return to this screen. Please press [ENTER] to proceed.\x1b[K\x1b[0m'
