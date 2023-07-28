168dc69fe791018758726c5538ba200933e93db6
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-28 13:04:54 -0500

    Create vmwarehorizon.sh

---
+++ vmwarehorizon.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/$USER
+sudo apt update -y
+sudo wget https://drive.google.com/uc?export=download&id=1sdcAnQvAii7ChOBiUgH55SsJFeexyFwQ&confirm=t&uuid=4cea855a-1e1e-4e30-a4ab-28533211d0cb&at=ALt4Tm2ItrMhxzUhLw1RTFy8O974:1690567085189
+sudo apt-get install VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb -y
