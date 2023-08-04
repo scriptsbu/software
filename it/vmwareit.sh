cfce2ffd97f234414f4e187e2fae62e40e5fb54b
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-04 16:19:44 -0500

    Update vmwareit.sh

--- it/vmwareit.sh
+++ it/vmwareit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 wget http://10.20.240.3/it/VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
-sudo dpkg -i VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb -y -f
-sleep 5
+sudo dpkg -i VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
 rm VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
 #--------------------------------------
 #VMware-Horizon-Client file stored at: t14-aus-it-server
