df0950a716c3976ee531bce9aeea58f53ca684c0
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-04 16:11:50 -0500

    Update papercutit.sh

--- it/papercutit.sh
+++ it/papercutit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget http://10.20.240.3/it/pc-print-deploy-client[papercut.torc.tech].deb ~/Downloads 
-sudo apt-get install ~/Downloads/pc-print-deploy-client[papercut.torc.tech].deb -y -f
+wget http://10.20.240.3/it/pc-print-deploy-client[papercut.torc.tech].deb
+sudo apt-get install pc-print-deploy-client[papercut.torc.tech].deb -y -f
 sleep 5
-rm ~/Downloads/pc-print-deploy-client[papercut.torc.tech].deb
+rm pc-print-deploy-client[papercut.torc.tech].deb
 /opt/PaperCutPrintDeployClient/initialise.sh -w 
 #--------------------------------------
 #Papercut file stored at: t14-aus-it-server
