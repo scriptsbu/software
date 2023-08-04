0cda8e61fce61f89caf2fd70c0fdbd2320fee95d
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-04 15:47:17 -0500

    Update openconnect.sh

--- openconnect.sh
+++ openconnect.sh
@@ -11,12 +11,12 @@ while true;do
     [Yy] ) 
       echo -e "\x1b[41;37m Alias will be "VPN"\x1b[K\x1b[0m"
       "alias vpn="sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push"" | sudo tee -a ~/.bashrc && 
-      sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push -f
+      sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push
       ;;
       
     * ) 
       echo -e "\x1b[41;37m Ok, Connecting to the VPN now.\x1b[K\x1b[0m"
-      sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push -f
+      sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push
       ;;
   esac
 done 
