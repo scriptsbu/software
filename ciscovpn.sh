29f659d07cc8085ffe8729a5ade63b5ae99c448a
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-04 14:42:01 -0500

    Update ciscovpn.sh

--- ciscovpn.sh
+++ ciscovpn.sh
@@ -5,3 +5,7 @@ sudo bash anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh && sleep 2 m &&
 sleep 10
 sudo apt-get update && apt-get upgrade -f -y
 exit
+
+
+#--------------------------------------
+#Cisco file stored at: https://github.com/scriptsbu/software/raw/main/storage/
