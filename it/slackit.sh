e0164242a0018a418d8ba8b9892d021272c6a7a8
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-04 16:15:00 -0500

    Update slackit.sh

--- it/slackit.sh
+++ it/slackit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget http://10.20.240.3/it/slack.deb ~/Downloads 
-sudo apt-get install ~/Downloads/slack.deb -y -f
+wget http://10.20.240.3/it/slack.deb
+sudo dpkg -i slack.deb -y -f
 sleep 5
-rm ~/Downloads/slack.deb -f
+rm slack.deb -f
 #--------------------------------------
 #Slack file stored at: t14-aus-it-server
