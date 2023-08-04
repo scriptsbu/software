cc3b15281fae41622995cd49c1fcedf5b7cd3c37
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-04 15:44:55 -0500

    Update and rename papercutvpn4.sh to papercutvpn5.sh

---
+++ papercutvpn5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push
+wget http://10.20.240.3/it/pc-print-deploy-client[papercut.torc.tech].deb ~/Downloads 
+sudo apt-get install ~/Downloads/pc-print-deploy-client[papercut.torc.tech].deb -y -f
+sleep 5
+rm ~/Downloads/pc-print-deploy-client[papercut.torc.tech].deb
+/opt/PaperCutPrintDeployClient/initialise.sh -w 
+#--------------------------------------
+#Papercut file stored at: t14-aus-it-server
