ef9f29103e7a35d1a9f9aece0a14705b0b07fccd
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-04 14:43:42 -0500

    Update vmwarehorizon.sh

--- vmwarehorizon.sh
+++ vmwarehorizon.sh
@@ -7,3 +7,7 @@ gdown https://drive.google.com/uc?id=1sdcAnQvAii7ChOBiUgH55SsJFeexyFwQ
 sudo apt-get install VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb -y
 sudo apt-get update && apt-get upgrade
 sudo rm VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb -f
+
+
+#--------------------------------------
+#VMware horizon file stored at: ScriptBU google drive
