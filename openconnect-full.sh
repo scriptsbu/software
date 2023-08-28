8c40984900e1809da00d63c84f5e513661742a75
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-28 09:22:57 -0500

    Update openconnect-full.sh

--- openconnect-full.sh
+++ openconnect-full.sh
@@ -10,13 +10,13 @@ while true;do
   case "${QUESTION}" in
     [Yy] ) 
       echo -e "\x1b[41;37m Alias will be "VPN"\x1b[K\x1b[0m"
-      "alias torcvpn='sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push --user=$USER && resetdns'alias resetdns='sudo systemd-resolve --flush-caches'" | sudo tee -a ~/.bashrc &&   "alias vpn="sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push"" | sudo tee -a ~/.bashrc 
-      sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push
+      "alias torcvpn='sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Full-Push --user=$USER && resetdns'alias resetdns='sudo systemd-resolve --flush-caches'" | sudo tee -a ~/.bashrc &&   "alias vpn="sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Full-Push"" | sudo tee -a ~/.bashrc 
+      sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Full-Push
       ;;
       
     * ) 
       echo -e "\x1b[41;37m Ok, Connecting to the VPN now.\x1b[K\x1b[0m"
-      sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Split-Push
+      sudo openconnect vpn.torcrobotics.com --authgroup=Employee-Full-Push
       ;;
   esac
 done 
