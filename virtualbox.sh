e41b74c20ff143439f91192151182d10f2892fd7
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-09-27 17:26:59 -0500

    Update virtualbox.sh

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo -e "\x1b[41;37mWhen prompted, Hit ESC key at the Pink screen and accept. Press ENTER to continue.\x1b[K\x1b[0m"
+read -p ""
 sudo apt-get update
 sudo apt-get install virtualbox -y -f
 sudo pkill virtualbox -f
