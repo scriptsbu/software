2b931a9e8768f98f58cc984a85c5714b20b69fb2
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-11-07 13:28:38 -0600

    Update u20-falcon.sh

--- u20-falcon.sh
+++ u20-falcon.sh
@@ -2,6 +2,7 @@
 cd ~\Downloads
 wget http://10.20.240.3/it/falcon-sensor_7.04.0-15907_amd64.deb
 sudo dpgk -i falcon-sensor_7.04.0-15907_amd64.deb
+sudo /opt/CrowdStrike/falconctl -s --cid=<6C145CE47FC14784904D6E1B2F9B5A4C-5F>
 service falcon-sensor start
 systemctl status falcon-sensor
 ps -e | grep falcon-sensor
