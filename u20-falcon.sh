e9c2663ee1e0438844befcd0a30e9c5b4d500b67
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-11-07 14:00:18 -0600

    Update u20-falcon.sh

--- u20-falcon.sh
+++ u20-falcon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ~/Downloads
 wget http://10.20.240.3/it/falcon-sensor_7.04.0-15907_amd64.deb
+sudo dpkg -i falcon-sensor_7.04.0-15907_amd64.deb
 wget http://10.20.240.3/it/script/u20-falcon-cid.sh
 bash u20-falcon-cid.sh
 sudo rm -r u20-falcon-cid.sh
