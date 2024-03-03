3321cbb03df0b2ce21aa691e0458bfce60f3ddf2
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-03-03 13:42:47 -0600

    Update gp-gui-web-18.04.sh

--- global-protect/gp-18.04/gp-gui-web-18.04.sh
+++ global-protect/gp-18.04/gp-gui-web-18.04.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo rm -r ~/Downloads/GlobalProtect_UI_focal_deb-6.1.2.0-82.deb -f
+sudo rm -r ~/Downloads/GlobalProtect_UI_deb-6.1.2.0-82.deb -f
 sudo rm -r ~/Downloads/gnome-browser-connector_42.1-4_all.deb -f
 sudo rm -r ~/Downloads/GlobalProtect_focal_deb-6.1.2.0-82.deb -f
 cd ~/Downloads/
 ##########################################################################################################
-#----DOWNLOADING: GlobalProtect_GUI_Installer/GlobalProtect_UI_focal_deb-6.1.2.0-82.deb
+#----DOWNLOADING: GlobalProtect_GUI_Installer/GlobalProtect_UI_deb-6.1.2.0-82.deb
 ##########################################################################################################
 echo -e "\x1b[41;37mA browser window will now open to download dependencies; return to Terminal after the download is completed.\x1b[K\x1b[0m"
 read -p "Press ENTER key to proceed with the download."
 xdg-open https://torcrobotics-my.sharepoint.com/:u:/r/personal/juan_molleda_torc_ai/Documents/Network-documents/GP-Ubuntu-18.04-Installer/GlobalProtect_UI_deb-6.1.2.0-82.deb?download=1
 sleep 1
 echo -e "\x1b[41;37mAfter downloading the .deb file, verify that the file exists in the downloads folder.\x1b[K\x1b[0m" && read -p "Press ENTER key to open instpect the folder."
-ls ~/Downloads | grep GlobalProtect_UI_focal_deb-6.1.2.0-82.deb
-read -p "Is GlobalProtect_UI_focal_deb-6.1.2.0-82.deb present in the directory? Press ENTER key to continue."
+ls ~/Downloads | grep GlobalProtect_UI_deb-6.1.2.0-82.deb
+read -p "Is GlobalProtect_UI_deb-6.1.2.0-82.deb present in the directory? Press ENTER key to continue."
 ##########################################################################################################
-if [ ! -f "GlobalProtect_UI_focal_deb-6.1.2.0-82.deb" ]; then
-   echo -e "\x1b[41;37mGlobalProtect_UI_focal_deb-6.1.2.0-82.deb not found, Please restart Script.\x1b[K\x1b[0m" &&
+if [ ! -f "GlobalProtect_UI_deb-6.1.2.0-82.deb" ]; then
+   echo -e "\x1b[41;37mGlobalProtect_UI_deb-6.1.2.0-82.deb not found, Please restart Script.\x1b[K\x1b[0m" &&
    exit
      fi
 ##########################################################################################################
 sudo apt-get install resolvconf
-sudo dpkg --force-all -i GlobalProtect_UI_focal_deb-6.1.2.0-82.deb; sudo apt-get -f install -y
+sudo dpkg --force-all -i GlobalProtect_UI_deb-6.1.2.0-82.deb; sudo apt-get -f install -y
 ##########################################################################################################
 #----ADDING: <default-browser>yes</default-browser> TO: /opt/paloaltonetworks/globalprotect/pangps.xml 
 ##########################################################################################################
@@ -39,7 +39,7 @@ sudo systemctl daemon-reload
 ###########################################################################################################
 sudo apt-get install chrome-gnome-shell -y -f
 sudo apt-get install gnome-tweak-tool -y -f 
-sudo rm -r ~/Downloads/GlobalProtect_UI_focal_deb-6.1.2.0-82.deb -f
+sudo rm -r ~/Downloads/GlobalProtect_UI_deb-6.1.2.0-82.deb -f
 echo -e "\x1b[41;37mInstall the GNOME Shell extension (if it's not already installed) by clicking on the “Click here to install browser extension” hyperlink. Continue and Add the GNOME Shell extension and return to Terminal. Press ENTER key to continue.\x1b[K\x1b[0m"
 read -p ""
 gnome-terminal -- google-chrome https://extensions.gnome.org/
