59fea716f762ade2762c00e58f96a9b6f4cf7a7b
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-03-03 13:24:30 -0600

    Update gp-cli-20.04.sh

--- global-protect/gp-20.04/gp-cli-20.04.sh
+++ global-protect/gp-20.04/gp-cli-20.04.sh
@@ -3,14 +3,12 @@ sudo apt-get update
 sudo rm -r ~/Downloads/GlobalProtect_UI_focal_deb-6.1.2.0-82.deb -f
 sudo rm -r ~/Downloads/gnome-browser-connector_42.1-4_all.deb -f
 sudo rm -r ~/Downloads/GlobalProtect_focal_deb-6.1.2.0-82.deb -f
-sudo apt-get install google-chrome-stable -y -f
 cd ~/Downloads/
 ##########################################################################################################
 #----DOWNLOADING: GlobalProtect_GUI_Installer/GlobalProtect_UI_focal_deb-6.1.2.0-82.deb
 ##########################################################################################################
 echo -e "\x1b[41;37mA browser window will now open to download dependencies; return to Terminal after the download is completed.\x1b[K\x1b[0m"
 read -p "Press ENTER key to proceed with the download."
-#DISABLED - gnome-terminal -- google-chrome https://torcrobotics-my.sharepoint.com/:u:/r/personal/juan_molleda_torc_ai/Documents/GlobalProtect_GUI_Installer/GlobalProtect_UI_focal_deb-6.1.2.0-82.deb?download=1 -e
 xdg-open https://torcrobotics-my.sharepoint.com/:u:/r/personal/juan_molleda_torc_ai/Documents/GlobalProtect_GUI_Installer/GlobalProtect_UI_focal_deb-6.1.2.0-82.deb?download=1
 sleep 1
 xdg-open https://torcrobotics-my.sharepoint.com/:u:/r/personal/juan_molleda_torc_ai/Documents/GlobalProtect_CLI_installer/GlobalProtect_focal_deb-6.1.2.0-82.deb?download=1
@@ -30,7 +28,6 @@ if [ ! -f "GlobalProtect_focal_deb-6.1.2.0-82.deb" ]; then
 ##########################################################################################################
 sudo apt-get install resolvconf
 sudo apt-get install gir1.2-gtk-3.0 gir1.2-webkit2-4.0
-#DISABLE - sudo dpkg --force-all -i GlobalProtect_UI_focal_deb-6.1.2.0-82.deb; sudo apt-get -f install -y
 sudo dpkg --force-all -i GlobalProtect_UI_focal_deb-6.1.2.0-82.deb; sudo apt-get -f install -y; sudo dpkg -i GlobalProtect_focal_deb-6.1.2.0-82.deb
 #globalprotect disconnect none
 ##########################################################################################################
@@ -47,25 +44,10 @@ sudo dpkg --force-all -i GlobalProtect_UI_focal_deb-6.1.2.0-82.deb; sudo apt-get
   sudo sysctl -p
 ###########################################################################################################
 sudo apt-get install gnome-tweak-tool -y -f
-#DISABLED - sudo apt install chrome-gnome-shell meson -y -f
 ###########################################################################################################
 #INSTALL GNOME SHELL EXTENSION MANNUALLY
 ###########################################################################################################
-#DISABLED - cd ~/Downloads
-#DISABLED - wget https://launchpad.net/ubuntu/+source/gnome-browser-connector/42.1-4/+build/26546591/+files/gnome-browser-connector_42.1-4_all.deb
-#DISABLED - sudo dpkg --force-all -i ~/Downloads/gnome-browser-connector_42.1-4_all.deb; sudo apt-get -f install -y
 sudo apt-get install chrome-gnome-shell -y -f
-#DISABLED - echo -e "\x1b[41;37mInstall the GNOME Shell extension (if it's not already installed) by clicking on the “Click here to install browser extension” hyperlink. Continue and Add the GNOME Shell extension and return to Terminal. Press ENTER key to continue.\x1b[K\x1b[0m"
-#DISABLED - gnome-terminal -- google-chrome https://extensions.gnome.org -e && read -p "Press ENTER key to continue."
-###########################################################################################################
-#INSTALL APP INDICATOR KSTATUS
-##########################################################################################################
-git clone https://github.com/ubuntu/gnome-shell-extension-appindicator.git
-meson gnome-shell-extension-appindicator /tmp/g-s-appindicators-build
-ninja -C /tmp/g-s-appindicators-build install
-sudo gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
-sudo gnome-extensions enable ubuntu-appindicators@ubuntu.com
-sudo rm ~/Downloads/gnome-shell-extension-appindicator
 sudo rm -r ~/Downloads/GlobalProtect_UI_focal_deb-6.1.2.0-82.deb
 sudo rm -r ~/Downloads/gnome-browser-connector_42.1-4_all.deb
 ###########################################################################################################
