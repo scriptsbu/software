c1b85664309d4af560f10dce74b11066128c76ec
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-03-03 14:24:19 -0600

    Update gp-cli-20.04.sh

--- global-protect/gp-20.04/gp-cli-20.04.sh
+++ global-protect/gp-20.04/gp-cli-20.04.sh
@@ -11,7 +11,7 @@ echo -e "\x1b[41;37mA browser window will now open to download dependencies; ret
 read -p "Press ENTER key to proceed with the download."
 xdg-open https://torcrobotics-my.sharepoint.com/:u:/r/personal/juan_molleda_torc_ai/Documents/GlobalProtect_GUI_Installer/GlobalProtect_UI_focal_deb-6.1.2.0-82.deb?download=1
 sleep 1
-read - p "PRESS ENTER KEY TO CONTINUE"
+read -p "PRESS ENTER KEY TO CONTINUE"
 xdg-open https://torcrobotics-my.sharepoint.com/:u:/r/personal/juan_molleda_torc_ai/Documents/GlobalProtect_CLI_installer/GlobalProtect_focal_deb-6.1.2.0-82.deb?download=1
 sleep 1
 echo -e "\x1b[41;37mAfter downloading the .deb file, verify that the file exists in the downloads folder.\x1b[K\x1b[0m" && read -p "Press ENTER key to open instpect the folder."
